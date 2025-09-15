9da8fd8bb9f611ef3430123e8ed09c707caf9e25
harshvardhan119 <harshvardhansinghha@gmail.com>
2025-09-15 18:32:22 +0530

    aded google auth and storing all google auth data in different mongodb

---
+++ src/googleauth/googleroutes.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const router = express.Router();
+const passport = require("../googleauth/googlestrategy");
+const jwt = require("jsonwebtoken");
+
+router.get("/google", passport.authenticate("google", { scope: ["profile", "email"] }));
+
+
+// router.get(
+//   "/google/callback",
+//   passport.authenticate("google", { failureRedirect: "/" }),
+//   (req, res) => {
+//     res.redirect("http://localhost:8080/dashboard"); 
+//   }
+// );
+router.get(
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/" }),
+  (req, res) => {
+    const token = jwt.sign(
+      { id: req.user._id, email: req.user.email },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    // redirect with token as query param
+    res.redirect(`http://localhost:8080/dashboard?token=${token}`);
+  }
+);
+
+router.get("/logout", (req, res) => {
+  req.logout(err => {
+    if (err) return next(err);
+    res.redirect("/");
+  });
+});
+
+module.exports = router;
