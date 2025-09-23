0be3a09b37a44bb644a9041f54072e35d0cefec6
Avinash Poojary <avinashpoojary651@gmail.com>
2025-09-23 15:57:36 +0530

    backend for image generation

--- src/routes/ai.js
+++ src/routes/ai.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const { handleGenerateScript, handleGenerateVideo } = require('../controllers/aiController');
+const { generateImage } = require('../controllers/imageGeneratorController');
+const authMiddleware = require('./authMiddleware');
 
 const router = express.Router();
 
 router.post('/generate-script', handleGenerateScript);
 router.post('/generate-video', handleGenerateVideo);
+
+router.post('/generate-image', authMiddleware, generateImage);
 
 module.exports = router;
