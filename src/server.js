03c2134384d7e4df4b02c500716b54f5d6a35f22
harshvardhan119 <harshvardhansinghha@gmail.com>
2025-09-26 17:37:25 +0530

    change made for cors

--- src/server.js
+++ src/server.js
@@ -95,11 +95,12 @@ mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
     console.log("Connected to MongoDB");
-    app.listen(PORT, () => {
-      console.log(`Server running on http://localhost:${PORT}`);
-    });
+    // app.listen(PORT, () => {
+    //   console.log(`Server running on http://localhost:${PORT}`);
+    // });
   })
   .catch((err) => {
     console.error("Failed to connect to MongoDB:", err);
     process.exit(1);
   });
+  module.exports = app
